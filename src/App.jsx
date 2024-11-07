3e82d2750e30ea567e8fbabbc1994845e027df4d
jmngandu <jamesmngandu@gmail.com>
2024-11-07 11:00:43 +0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [color, setColor] = useState("blue");
+
+  let handleChangeColor = () => {
+    setColor("green");
+  };
+  return (
+    <>
+      <h1>I like color {color}</h1>
+      <button onClick={handleChangeColor}>change</button>
+    </>
+  );
 }
 
 export default App;
