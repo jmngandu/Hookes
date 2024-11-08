a272168da51674835fc70bac6cdf4bcbebb8ffc0
jmngandu <jamesmngandu@gmail.com>
2024-11-08 12:07:43 +0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import EffectHook from "./pages/effectHook";
+import RefHook from "./pages/refHook";
 import StateHook from "./pages/stateHook";
 
 function App() {
   return (
     <>
       <StateHook />
-
       <EffectHook />
+      <RefHook />
     </>
   );
 }
---
+++ src/pages/refHook.jsx
@@ -0,0 +1,8 @@
+const RefHook = () => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
+export default RefHook;
