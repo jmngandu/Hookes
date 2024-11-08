2bb9edbaf4ed28f3b3dc3725eadef2b62c5b4a8d
jmngandu <jamesmngandu@gmail.com>
2024-11-08 12:21:33 +0300

    hooks

--- src/pages/refHook.jsx
+++ src/pages/refHook.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const RefHook = () => {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  let handleIncre = () => {
+    setCount((count) => count + 1);
+    setValue((value) => value - 1);
+  };
+
+  let handleDecre = () => {
+    setCount((count) => count - 1);
+    setValue((value) => value + 1);
+  };
   return (
     <>
-      <h1>hello</h1>
+      <button onClick={handleIncre}>-1</button>
+      <h1>{value}</h1>
+      <h1>{count}</h1>
+      <button onClick={handleDecre}>+1</button>
     </>
   );
 };
