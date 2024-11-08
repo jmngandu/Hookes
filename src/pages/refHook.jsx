85e14597cb62f18a64c72d076b88a9db749a1e50
jmngandu <jamesmngandu@gmail.com>
2024-11-08 12:40:59 +0300

    hooks

--- src/pages/refHook.jsx
+++ src/pages/refHook.jsx
@@ -8,8 +8,8 @@ const RefHook = () => {
     <>
       <button onClick={() => setValue((prev) => prev - 1)}>-1</button>
       <h1>{value}</h1>
-      <h1>{count}</h1>
       <button onClick={() => setValue((prev) => prev + 1)}>+1</button>
+      <h1>Render Count: {count}</h1>
     </>
   );
 };
