853fed25df93b38d440ba55e2ba777e60b292de8
jmngandu <jamesmngandu@gmail.com>
2024-11-08 12:57:05 +0300

    hooks

--- src/pages/refHook.jsx
+++ src/pages/refHook.jsx
@@ -1,14 +1,19 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const RefHook = () => {
   const [value, setValue] = useState(0);
   const count = useRef(0);
+  const inputEl = useRef();
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
   return (
     <>
       <button onClick={() => setValue((prev) => prev - 1)}>-1</button>
       <h1>{value}</h1>
       <button onClick={() => setValue((prev) => prev + 1)}>+1</button>
       <h1>Render Count: {count.current}</h1>
+      <input type="text" ref={inputEl} />
     </>
   );
 };
