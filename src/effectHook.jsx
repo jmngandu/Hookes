84ccb5fbbe5a0c27b88e9bc15ac77eb8d029a1c8
jmngandu <jamesmngandu@gmail.com>
2024-11-08 12:02:14 +0300

    hook

--- src/effectHook.jsx
+++ src/effectHook.jsx
@@ -5,7 +5,7 @@ function EffectHook() {
     setTimeout(() => {
       setCount((count) => count + 1);
     }, 2000);
-  });
+  }, [count]);
   return (
     <div>
       <h1>i rendered {count} times!</h1>
